58f5d2b4ade661cf83031210ceb4b1d3dc01ff1f
vaneet <vaneetkataria54@gmail.com>
2018-12-07 14:15:15 +0530

    Added initial project for JpaToHibernate migration

---
+++ jdbcToJpaMigration/src/main/java/com/katariasoft/technologies/jpaHibernate/JpaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.katariasoft.technologies.jpaHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateApplication.class, args);
+	}
+}
