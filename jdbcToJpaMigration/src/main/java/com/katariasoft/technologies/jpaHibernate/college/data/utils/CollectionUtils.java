abe4a7573afcd057b045c19256eba5603d7c0816
vaneet <vaneetkataria54@gmail.com>
2019-01-07 12:46:06 +0530

    Refactored code

--- jdbcToJpaMigration/src/main/java/com/katariasoft/technologies/jpaHibernate/college/data/utils/CollectionUtils.java
+++ jdbcToJpaMigration/src/main/java/com/katariasoft/technologies/jpaHibernate/college/data/utils/CollectionUtils.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
 import org.springframework.util.Assert;
 
 public class CollectionUtils {
