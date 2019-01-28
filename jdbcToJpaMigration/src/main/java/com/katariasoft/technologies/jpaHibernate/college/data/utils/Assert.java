94331dde57ad3114f6cb48f52fa870047a98e9b8
vaneet <vaneetkataria54@gmail.com>
2019-01-28 14:17:48 +0530

    Utility to throw passed exception with passed cause.

---
+++ jdbcToJpaMigration/src/main/java/com/katariasoft/technologies/jpaHibernate/college/data/utils/Assert.java
@@ -0,0 +1,20 @@
+package com.katariasoft.technologies.jpaHibernate.college.data.utils;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+
+public class Assert {
+
+	public static <X extends Throwable> void isTrue(boolean expression, Function<String, ? extends X> execptionSupplier,
+			String message) throws X {
+		if (!expression)
+			throw execptionSupplier.apply(message);
+	}
+
+	@Test
+	public void assertTest() {
+		Assert.isTrue(5 < 3, RuntimeException::new, "Input must be less than three");
+	}
+
+}
